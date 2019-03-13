4dd0da8dddec453a69b718a8519eab2df5868c8d
linyang <nyangyang@mifengkong.cn>
2019-03-13 20:14:21 +0800

    JWT未完待续

---
+++ src/main/java/cn/qiuzhizhushou/wechat/controller/JWTController.java
@@ -0,0 +1,16 @@
+package cn.qiuzhizhushou.wechat.controller;
+
+import com.auth0.jwt.JWTVerifier;
+
+/**
+ * Created by IDEA.
+ * User: mc
+ * Date: 19/3/13
+ * Time: 上午9:27
+ */
+public class JWTController extends BaseController
+{
+    public void test()
+    {
+    }
+}
